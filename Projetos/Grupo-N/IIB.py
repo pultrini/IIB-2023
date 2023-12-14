ca27b526a5397e7ba235484a46ab48437b18b2c4
pultrini <89112736+pultrini@users.noreply.github.com>
2023-12-13 23:06:39 -0300

    Add os códigos utilizados pelo grupo-N por favor
    Este são os codigos do grupo que realizou a apresentação do projeto sobre banco de dados com cartão RFID

---
+++ Projetos/Grupo-N/IIB.py
@@ -0,0 +1,35 @@
+import serial
+import subprocess
+import webbrowser
+
+porta_serial = serial.Serial('COM3', 115200)
+
+try:
+    while True:
+        # Ler dados da porta serial
+        dado = porta_serial.readline().decode().strip()
+        print(f"Dado recebido: {dado}")
+        cardUI = dado
+except KeyboardInterrupt:
+    name = input("Digite seu Nome: ")
+    sex = input("Sexo: ")
+    nasc = input("Data de Nascimento: ")
+    #alergia = input("alergia a algum medicamento: ")
+    sang = input("Tipo Sanguineo: ")
+    #funcao = input("Sua funcao: ")     
+    porta_serial.close()
+    print("Comunicação encerrada.")
+
+
+url = 'https://script.google.com/macros/s/AKfycbzFojG0AfGlXALAFHxjaAyhyIggTS6FTgEkMMkyTCN4zp5ZB9covFtYNX_LBbodsgkiOg/exec' 
+url2 = "?value1={}&value2={}&value3={}&value4={}&value5={}".format(cardUI, name, sex, nasc, sang)
+
+url_final=url + url2
+
+print(url2)
+
+print(url_final)
+
+
+# Abrir a URL no navegador padrão
+webbrowser.open_new_tab(url_final)
